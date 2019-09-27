9272850c5a24441ce8e3fbbf919e72c6d767d38c
Mike Townsley <mike@madllama.net>
2019-09-27 07:59:09 -0500

    Re-worked LCD display, fixed backlight

--- src/hardware.py
+++ src/hardware.py
@@ -19,55 +19,92 @@ from src.events import EventBus
 class HD44780Display(GenericLcdDisplay):
     """ An HD44780 character based LCD display fronted by an i2c interface """
 
-    # Modified version of the driver from
-    # https://github.com/sunfounder/SunFounder_SensorKit_for_RPi2.git
-    # Command reference
-    # https://mil.ufl.edu/3744/docs/lcdmanual/commands.html
+    # commands
+    LCD_CLEARDISPLAY = 0x01
+    LCD_RETURNHOME = 0x02
+    LCD_ENTRYMODESET = 0x04
+    LCD_DISPLAYCONTROL = 0x08
+    LCD_CURSORSHIFT = 0x10
+    LCD_FUNCTIONSET = 0x20
+    LCD_SETCGRAMADDR = 0x40
+    LCD_SETDDRAMADDR = 0x80
+
+    # flags for display entry mode
+    LCD_ENTRYRIGHT = 0x00
+    LCD_ENTRYLEFT = 0x02
+    LCD_ENTRYSHIFTINCREMENT = 0x01
+    LCD_ENTRYSHIFTDECREMENT = 0x00
+
+    # flags for display on/off control
+    LCD_DISPLAYON = 0x04
+    LCD_DISPLAYOFF = 0x00
+    LCD_CURSORON = 0x02
+    LCD_CURSOROFF = 0x00
+    LCD_BLINKON = 0x01
+    LCD_BLINKOFF = 0x00
+
+    # flags for display/cursor shift
+    LCD_DISPLAYMOVE = 0x08
+    LCD_CURSORMOVE = 0x00
+    LCD_MOVERIGHT = 0x04
+    LCD_MOVELEFT = 0x00
+
+    # flags for function set
+    LCD_8BITMODE = 0x10
+    LCD_4BITMODE = 0x00
+    LCD_2LINE = 0x08
+    LCD_1LINE = 0x00
+    LCD_5x10DOTS = 0x04
+    LCD_5x8DOTS = 0x00
+
+    # flags for backlight control
+    LCD_BACKLIGHT = 0x08
+    LCD_NOBACKLIGHT = 0x00
+
+    CTRL_EN = 0b00000100  # Enable bit
+    CTRL_RW = 0b00000010  # Read/Write bit
+    CTRL_RS = 0b00000001  # Register select bit
+
     def __init__(self, addr, width, height):
         super().__init__(width, height)
+        self.__addr = addr
+        self.__bus = smbus.SMBus(1)
+        self.__backlightState = self.LCD_BACKLIGHT
 
-        self.__smbus = smbus.SMBus(1)
-        self.__lcdAddr = addr
-        self.__blen = 0  # Hard-coding this, not sure what other options do
+        self.__write(0x03)
+        self.__write(0x03)
+        self.__write(0x03)
+        self.__write(0x02)
 
-        self.__send_command(0x33)  # Must initialize to 8-line mode at first
-        sleep(0.005)
-        self.__send_command(0x32)  # Then initialize to 4-line mode
-        sleep(0.005)
-        self.__send_command(0x28)  # 2 Lines & 5*7 dots
-        sleep(0.005)
-        self.__send_command(0x0C)  # Enable display without cursor
-        sleep(0.005)
+        self.__write(
+            self.LCD_FUNCTIONSET | self.LCD_2LINE |
+            self.LCD_5x8DOTS | self.LCD_4BITMODE)
+        self.__write(self.LCD_DISPLAYCONTROL | self.LCD_DISPLAYON)
+        self.__write(self.LCD_CLEARDISPLAY)
+        self.__write(self.LCD_ENTRYMODESET | self.LCD_ENTRYLEFT)
+        sleep(0.2)
 
-        self.clear()
-        self.setBacklight(True)
+    def __write_cmd(self, cmd):
+        self.__bus.write_byte(self.__addr, cmd)
+        sleep(0.0001)
 
-    def __write_word(self, data):
-        temp = data
-        if self.__blen == 1:
-            temp |= 0x08
-        else:
-            temp &= 0xF7
-        self.__smbus.write_byte(self.__lcdAddr, temp)
+    # clocks EN to latch command
+    def __strobe(self, data):
+        self.__write_cmd(data | self.CTRL_EN | self.__backlightState)
+        sleep(.0005)
+        self.__write_cmd(((data & ~self.CTRL_EN) | self.__backlightState))
+        sleep(.0001)
 
-    def __send(self, buf, op):
-        buf |= op                # RS = 0, RW = 0, EN = 1
-        self.__write_word(buf)
-        sleep(0.002)
-        buf &= 0xFB               # Make EN = 0
-        self.__write_word(buf)
+    def __write_four_bits(self, data):
+        self.__write_cmd(data | self.__backlightState)
+        self.__strobe(data)
 
-    def __send_command(self, comm):
-        # Send last 4 bits first, then send the first 4 bits
-        self.__send(comm & 0xF0, 0x04)
-        self.__send((comm & 0x0f) << 4, 0x04)
+    # write a command to lcd
+    def __write(self, cmd, mode=0):
+        self.__write_four_bits(mode | (cmd & 0xF0))
+        self.__write_four_bits(mode | ((cmd << 4) & 0xF0))
 
-    def __send_data(self, data):
-        # Send last 4 bits first, then send the first 4 bits
-        self.__send(data & 0xF0, 0x05)
-        self.__send((data & 0x0f) << 4, 0x05)
-
-    def __write(self, x, y, str):
+    def __write_str(self, x, y, str):
         x = min(self.width-1, max(0, x))
         y = min(self.height-1, max(0, y))
 
@@ -76,30 +113,31 @@ class HD44780Display(GenericLcdDisplay):
             addr = 0x80 + 0x40 * y + x
         else:
             addr = 0x80 + self.width + 0x40 * (y-2) + x
-        self.__send_command(addr)
+        self.__write(addr)
 
-        for chr in str:
-            self.__send_data(ord(chr))
+        for char in str:
+            self.__write(ord(char), self.CTRL_RS)
 
     def clear(self):
         """ Clear the contents of the LCD screen """
-        self.__send_command(0x01)  # Clear Screen
+        self.__write(self.LCD_CLEARDISPLAY)
+        self.__write(self.LCD_RETURNHOME)
 
     def setBacklight(self, enabled: bool):
         """ If enabled, turns on the backlight, otherwise turns it off """
         if enabled:
-            # self.__send_command(0x12)
-            self.__smbus.write_byte(self.__lcdAddr, 0x08)
+            self.__backlightState = self.LCD_BACKLIGHT
+            self.__write_cmd(self.LCD_BACKLIGHT)
         else:
-            # self.__send_command(0x08)
-            self.__smbus.write_byte(self.__lcdAddr, 0x00)
+            self.__backlightState = self.LCD_NOBACKLIGHT
+            self.__write_cmd(self.LCD_NOBACKLIGHT)
 
     def commit(self):
         """ Commits all pending changes to the display """
         results = super().commit()
         for i in range(len(results)):
             for change in results[i]:
-                self.__write(change[0], i, change[1])
+                self.__write_str(change[0], i, change[1])
 
 
 class GpioPushButton(GenericButton):
