c440451aba703cd06a99aa45cbac7d36f8ff2187
Mike Townsley <mike@madllama.net>
2020-04-11 18:38:43 -0500

    updates

---
+++ scripts/rpt.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import re
+import argparse
+
+from subprocess import call, check_output
+from threading import Thread
+from io import StringIO
+from queue import Queue, Empty
+
+
+class RptLauncher:
+    ALL_HOSTTYPES = {
+        'x86_64': 'x86_64',
+        'armv7l': 'arm',
+        'armv6l': 'arm'
+    }
+
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            description='Central interface for interacting w/ rpt containers')
+        parser.add_argument(
+            '--build', nargs=argparse.REMAINDER,
+            help='Build the rpt containers locally')
+        parser.add_argument(
+            '--run', nargs=argparse.REMAINDER,
+            help='Run the rpt orchestration')
+        parser.add_argument(
+            '--dryrun', action='store_true', default=False,
+            help='Only print commands, do not execute')
+        parser.add_argument(
+            '--debug', action='store_true', default=False,
+            help='Add debugging output')
+
+        self.args = parser.parse_args()
+
+    def shell(self, arglist):
+        if self.args.debug:
+            print(f"SHELL: {arglist}")
+
+        if not self.args.dryrun:
+            return call(arglist)
+
+        return 0
+
+    def execute(self):
+        uname = check_output(['uname', '-m']).decode('UTF-8').rstrip()
+        hosttype = self.ALL_HOSTTYPES.get(uname)
+        if hosttype is None:
+            raise Exception(f"Host with uname {uname} is not supported")
+
+        print(self.args.run)
+        print(self.args.build)
+
+        baseComposeArgs = [
+            'docker-compose',
+            '--file', 'docker/docker-compose.yaml',
+            '--env-file', f"docker/docker-hosttype-{hosttype}.env"
+        ]
+        rptStartArgs = ['python3', '-m', 'src']
+
+        if self.args.run is not None:
+            rptStartArgs = rptStartArgs + self.args.run
+            return self.shell(
+                arglist=baseComposeArgs + ['run', 'rpt'] + rptStartArgs)
+
+        if self.args.build is not None:
+            rptStartArgs = rptStartArgs + self.args.build
+            return self.shell(
+                arglist=baseComposeArgs + ['build', 'rpt'] + rptStartArgs)
+
+
+if __name__ == "__main__":
+    sys.exit(RptLauncher().execute())
