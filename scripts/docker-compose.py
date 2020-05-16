7b78870ac8d7b37737c3bf6d77e8ea227378b807
Mike Townsley <mike@madllama.net>
2020-05-16 16:03:20 -0500

    move to grafana provisioning

--- scripts/docker-compose.py
+++ scripts/docker-compose.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from os import environ, execve, path
+from os import environ, execve
 from sys import argv
 from subprocess import check_output
 
@@ -18,7 +18,7 @@ ENV_BY_HOSTTYPE = {
         'HOSTTYPE_IMAGE_GRAFANA': 'grafana/grafana:latest',
         'HOSTTYPE_IMAGE_POSTGRES': 'postgres:12.2',
         'NODEJS_VERSION': 'node-v11.15.0-linux-x64',
-        'GRAFANA_VERSION': 'grafana-rpi_6.7.3_amd64.deb'
+        'GRAFANA_VERSION': 'grafana_6.7.3_amd64.deb'
     },
     'arm': {
         'HOSTTYPE': 'arm',
@@ -28,20 +28,18 @@ ENV_BY_HOSTTYPE = {
             '64d120225e34c5829b87a9f28dc179c66a9ff2',
         'HOSTTYPE_IMAGE_POSTGRES': 'arm32v6/postgres:12.2-alpine',
         'NODEJS_VERSION': 'node-v11.15.0-linux-armv6l',
-        'GRAFANA_VERSION': 'grafana-rpi_6.7.3_armhf.deb'
+        'GRAFANA_VERSION': 'grafana_6.7.3_armhf.deb'
     }
 }
 
+# Not currently using this but could in the future
 ENV_BY_OSNAME = {
     'Darwin': {
-        'POSTGRES_DATA': '/private/var/lib/postgresql/data',
-        'GRAFANA_DATA': '/private/var/lib/grafana'
     },
     'Linux': {
-        'POSTGRES_DATA': '/var/lib/postgresql/data',
-        'GRAFANA_DATA': '/var/lib/grafana'
     }
 }
+
 osName = check_output(['uname', '-s']).decode('UTF-8').rstrip()
 osEnv = ENV_BY_OSNAME.get(osName)
 if osEnv is None:
@@ -51,18 +49,6 @@ uname = check_output(['uname', '-m']).decode('UTF-8').rstrip()
 hosttype = ALL_HOSTTYPES.get(uname)
 if hosttype is None:
     raise Exception(f"Host with uname {uname} is not supported")
-
-checkDir = ENV_BY_OSNAME[osName]['POSTGRES_DATA']
-if not path.isdir(checkDir):
-    raise Exception(
-        f"Directory {checkDir} must exist and be "
-        "writable by docker user")
-
-checkDir = ENV_BY_OSNAME[osName]['GRAFANA_DATA']
-if not path.isdir(checkDir):
-    raise Exception(
-        f"Directory {checkDir} must exist and be "
-        "writable by docker user")
 
 environment = environ.copy()
 for key, value in ENV_BY_HOSTTYPE[hosttype].items():
