8a97f043c52f78c1eee29f9270d1c5f5d7d1bbe4
Mike Townsley <mike@madllama.net>
2020-12-22 19:24:57 +0000

    some display progress

--- src/core/generics/GenericUserInterfaceV2.py
+++ src/core/generics/GenericUserInterfaceV2.py
@@ -3,10 +3,9 @@ from PIL import ImageFont, ImageDraw, Image
 import qrcode
 
 from src.core import ServiceConsumer, ServiceProvider, EventBus, Event
-from src.services import ThermostatService
+from src.services.ThermostatService import ThermostatService
 from src.core.events import ThermostatStateChangedEvent, \
-    SensorDataChangedEvent, PowerPriceChangedEvent, \
-    SettingsChangedEvent
+    SensorDataChangedEvent, PowerPriceChangedEvent
 
 
 class GenericUserInterfaceV2(ServiceConsumer):
@@ -31,13 +30,13 @@ class GenericUserInterfaceV2(ServiceConsumer):
         self._image = Image.new('1', (width, height), 255)
         self._draw = ImageDraw.Draw(self._image)
 
+        self._lastState = 'NONE'
+
     def setServiceProvider(self, provider: ServiceProvider):
         super().setServiceProvider(provider)
 
         eventBus = self._getService(EventBus)
 
-        eventBus.installEventHandler(
-            SettingsChangedEvent, self.__settingsChanged)
         eventBus.installEventHandler(
             SensorDataChangedEvent, self.__sensorDataChanged)
         eventBus.installEventHandler(
@@ -63,9 +62,6 @@ class GenericUserInterfaceV2(ServiceConsumer):
         elif event.button == GenericUserInterfaceV2.Button.ENTER:
             pass
 
-    def __settingsChanged(self, event: SettingsChangedEvent):
-        pass
-
     def __stateChanged(self, event: ThermostatStateChangedEvent):
         self.redraw()
 
@@ -76,33 +72,73 @@ class GenericUserInterfaceV2(ServiceConsumer):
     def image(self):
         return self._image
 
-    def redraw(self):
+    def drawCurrentTemp(self, temperature: float):
+        # https://pillow.readthedocs.io/en/stable/handbook/text-anchors.html
+        # Draw a bounding box that I'm expecting resembles the inset for
+        # the rectangle port in the case
+        pad = 2
+        fontName = 'Hack-Bold.ttf'
+        thermostatService = self._getService(ThermostatService)
 
-        def get_text_dimensions(text_string, font):
-            # https://stackoverflow.com/a/46220683/9263761
+        def getMetrics(text, font):
             ascent, descent = font.getmetrics()
+            (width, baseline), (offset_x, offset_y) = \
+                font.font.getsize(str(text))
+            return (width, baseline, offset_x, offset_y, ascent, descent)
 
-            text_width = font.getmask(text_string).getbbox()[2]
-            text_height = font.getmask(text_string).getbbox()[3]
+        # Draw the temperature with smaller fractional 10th in lower-right
+        tempWhole, tempFrac = divmod(int(temperature*10), 10)
+        fontWhole = ImageFont.truetype(fontName, 82)
+        fontWholeMetrics = getMetrics(str(tempWhole), fontWhole)
+        fontWholeLoc = (
+            pad, self._image.height/2 +
+            (fontWholeMetrics[4]-fontWholeMetrics[3])/2)
 
-            return (text_width, text_height, ascent-text_height)
+        fontFrac = ImageFont.truetype(fontName, 22)
+        fontFracMetrics = getMetrics(f".{tempFrac}", fontFrac)
+        fontFracLoc = (fontWholeLoc[0]+fontWholeMetrics[0]-4, fontWholeLoc[1])
 
-        temperature = '72'
-        font = ImageFont.truetype("segoe-ui.ttf", 72)
-        size = get_text_dimensions(temperature, font)
-        fontLoc = (8, self._image.height/2 - size[1]/2)
-        self._draw.rectangle([8, 8, 296-8, 128-8], fill=0)
-        self._draw.rectangle([9, 9, 296-9, 128-9], fill=255)
-        self._draw.rectangle(
-            [fontLoc[0], fontLoc[1],
-             fontLoc[0]+size[0], fontLoc[1]+size[1]], fill=0)
-        self._draw.rectangle(
-            [fontLoc[0]+1, fontLoc[1]+1,
-             fontLoc[0]+size[0]-1, fontLoc[1]+size[1]-1], fill=255)
+        self._draw.text(
+            fontWholeLoc, str(tempWhole), fill=0, anchor='ls', font=fontWhole)
+        self._draw.text(
+            fontFracLoc, f".{tempFrac}", fill=0, anchor='ls', font=fontFrac)
 
-        fontLoc = (fontLoc[0], fontLoc[1]-size[2])
-        self._draw.text(fontLoc, temperature, fill=0, font=font)
+        # Draw the current state
+        stateStr = str(thermostatService.state)
+        fontState = ImageFont.truetype(fontName, 16)
+        fontStateMetrics = getMetrics(stateStr, fontState)
+        fontStateBox = (
+            (0, 0),
+            (fontWholeMetrics[0] + fontFracMetrics[0] - 4,
+             fontStateMetrics[4]-fontStateMetrics[3]+8)
+        )
+        fontStateLoc = (
+            (fontStateBox[0][0]+fontStateBox[1][0])/2,
+            (fontStateBox[0][1]+fontStateBox[1][1])/2)
 
+        self._draw.rectangle(fontStateBox, fill=0)
+        self._draw.text(
+            fontStateLoc, stateStr, fill=255, anchor='mm', font=fontState)
+
+        # Draw the temperature target
+        targetStr = 'Target: 72'
+        fontTarget = ImageFont.truetype(fontName, 16)
+        fontTargetMetrics = getMetrics(targetStr, fontTarget)
+        fontTargetBox = (
+            (0, self._image.height -
+             (fontTargetMetrics[4]-fontTargetMetrics[3]+8)),
+            (fontWholeMetrics[0] + fontFracMetrics[0] - 4,
+             self._image.height)
+        )
+        fontTargetLoc = (
+            (fontTargetBox[0][0]+fontTargetBox[1][0])/2,
+            (fontTargetBox[0][1]+fontTargetBox[1][1])/2)
+
+        self._draw.rectangle(fontTargetBox, fill=0)
+        self._draw.text(
+            fontTargetLoc, targetStr, fill=255, anchor='mm', font=fontTarget)
+
+    def drawQrCode(self):
         qr = qrcode.QRCode(
             version=1, error_correction=qrcode.constants.ERROR_CORRECT_L,
             box_size=3, border=4,
@@ -112,5 +148,8 @@ class GenericUserInterfaceV2(ServiceConsumer):
         img = qr.make_image(
             fill_color="black", back_color="white").get_image()
         self._image.paste(img, (self._image.width-img.size[0], 0))
+
+    def redraw(self):
+        self.drawCurrentTemp(72.5)
 
         # epd.display(epd.getbuffer(self._image))
