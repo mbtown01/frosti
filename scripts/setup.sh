cc9b932a2031a73a39f2a8ad3da3d570132bcf76
Mike Townlsey <mike@madllama.net>
2020-12-31 13:07:29 -0600

    more setup fixes, twice-tested

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,7 +13,6 @@ OPT_SSID=''
 OPT_PASSPHRASE=''
 OPT_WIFI_COUNTRY='US'
 
-
 read -r -d '' FROSTI_PACKAGES <<-EOLIST
 	curl
 	fonts-hack-ttf
@@ -41,65 +40,64 @@ read -r -d '' FROSTI_PACKAGES <<-EOLIST
 	zlib1g-dev
 EOLIST
 
-
 read -r -d '' FROSTI_REQUIREMENTS <<-EOLIST
-	Adafruit-Blinka==5.9.1
-	adafruit-circuitpython-74hc595==1.2.1
-	adafruit-circuitpython-bme280==2.5.1
-	adafruit-circuitpython-bmp280==3.2.3
-	adafruit-circuitpython-busdevice==5.0.1
-	adafruit-circuitpython-charlcd==3.3.4
-	adafruit-circuitpython-mcp230xx==2.4.2
-	Adafruit-PlatformDetect==2.23.0
-	Adafruit-PureIO==1.1.8
-	aniso8601==8.1.0
-	attrs==20.3.0
-	autopep8==1.5.4
-	certifi==2020.12.5
-	chardet==4.0.0
-	click==7.1.2
-	flake8==3.8.4
-	Flask==1.1.2
-	Flask-Cors==3.0.9
-	flask-restx==0.2.0
-	idna==2.10
-	importlib-metadata==3.3.0
-	iniconfig==1.1.1
-	itsdangerous==1.1.0
-	Jinja2==2.11.2
-	jsonschema==3.2.0
-	MarkupSafe==1.1.1
-	mccabe==0.6.1
-	packaging==20.8
-	Pillow==8.0.1
-	pluggy==0.13.1
-	psycopg2-binary==2.8.6
-	ptvsd==4.3.2
-	py==1.10.0
-	pycodestyle==2.6.0
-	pyflakes==2.2.0
-	pyftdi==0.52.0
-	pyparsing==2.4.7
-	pyrsistent==0.17.3
-	pyserial==3.5
-	pytest==6.2.1
-	pytz==2020.4
-	pyusb==1.1.0
-	PyYAML==5.3.1
-	qrcode==6.1
-	requests==2.25.1
-	rpi-ws281x==4.2.5
-	RPi.GPIO==0.7.0
-	six==1.15.0
-	spidev==3.5
-	SQLAlchemy==1.3.22
-	SQLAlchemy-Utils==0.36.8
-	sysv-ipc==1.0.1
-	toml==0.10.2
-	typing-extensions==3.7.4.3
-	urllib3==1.26.2
-	Werkzeug==1.0.1
-	zipp==3.4.0
+	Adafruit-Blinka
+	adafruit-circuitpython-74hc595
+	adafruit-circuitpython-bme280
+	adafruit-circuitpython-bmp280
+	adafruit-circuitpython-busdevice
+	adafruit-circuitpython-charlcd
+	adafruit-circuitpython-mcp230xx
+	Adafruit-PlatformDetect
+	Adafruit-PureIO
+	aniso8601
+	attrs
+	autopep8
+	certifi
+	chardet
+	click
+	flake8
+	Flask
+	Flask-Cors
+	flask-restx
+	idna
+	importlib-metadata
+	iniconfig
+	itsdangerous
+	Jinja2
+	jsonschema
+	MarkupSafe
+	mccabe
+	packaging
+	Pillow
+	pluggy
+	psycopg2-binary
+	ptvsd
+	py
+	pycodestyle
+	pyflakes
+	pyftdi
+	pyparsing
+	pyrsistent
+	pyserial
+	pytest
+	pytz
+	pyusb
+	PyYAML
+	qrcode
+	requests
+	rpi-ws281x
+	RPi.GPIO
+	six
+	spidev
+	SQLAlchemy
+	SQLAlchemy-Utils
+	sysv-ipc
+	toml
+	typing-extensions
+	urllib3
+	Werkzeug
+	zipp
 EOLIST
 
 is_pi () {
@@ -152,12 +150,12 @@ run_and_mark_completed() {
 }
 
 do_setup_wifi() {
-  sudo raspi-config nonint do_wifi_country ${OPT_WIFI_COUNTRY}
-  sudo raspi-config nonint do_wifi_ssid_passphrase ${OPT_SSID} ${OPT_PASSPHRASE}
+  raspi-config nonint do_wifi_country ${OPT_WIFI_COUNTRY}
+  raspi-config nonint do_wifi_ssid_passphrase ${OPT_SSID} ${OPT_PASSPHRASE}
 }
 
 do_setup_file_systems() {
-  zz_rootsize=3002
+  zz_rootsize=4096
   zz_localdev=/dev/mmcblk0
   zz_newdir=/data
 
@@ -172,62 +170,63 @@ do_setup_file_systems() {
 }
 
 do_setup_services() {
-  sudo raspi-config nonint do_spi 1
-  sudo raspi-config nonint do_i2c 1
-  sudo raspi-config nonint do_ssh 1
-  sudo service ssh restart
+  raspi-config nonint do_spi 0
+  raspi-config nonint do_i2c 0
+  raspi-config nonint do_ssh 0
+  systemctl enable ssh
+  service ssh restart
 }
 
 do_localize_us() {
-  sudo raspi-config nonint do_configure_keyboard 'us'
-  sudo raspi-config nonint do_change_locale 'en_US.UTF-8'
-  sudo raspi-config nonint do_change_timezone 'US/Central'
+  raspi-config nonint do_configure_keyboard 'us'
+  raspi-config nonint do_change_locale 'en_US.UTF-8'
+  raspi-config nonint do_change_timezone 'US/Central'
 }
 
 do_setup_hostname() {
-  sudo raspi-config nonint do_hostname ${OPT_HOSTNAME}
+  raspi-config nonint do_hostname ${OPT_HOSTNAME}
 }
 
 do_install_packages_core() {
   # Install the core packages we need to get things going
-  sudo apt update && sudo apt upgrade -y
-  sudo apt update && sudo apt upgrade -y || return 1
-  sudo apt install -y git || return 1
+  apt update && apt upgrade -y
+  apt update && apt upgrade -y || return 1
+  apt install -y git || return 1
 }
 
 do_install_frosti_source() {
   # Once things are setup, can now pull down frosti and start configuring
   # the environment
   mkdir -pf ${FROSTI_HOME}
-  sudo git clone https://github.com/mbtown01/frosti.git ${FROSTI_HOME}
+  git clone https://github.com/mbtown01/frosti.git ${FROSTI_HOME}
 }
 
 do_install_packages() {
   cd ${FROSTI_HOME}
-  sudo apt install -y ${FROSTI_PACKAGES} || return 1
-  sudo python3 -m pip install ${FROSTI_REQUIREMENTS} || return 1
+  apt install -y ${FROSTI_PACKAGES} || return 1
+  python3 -m pip install ${FROSTI_REQUIREMENTS} || return 1
 }
 
 do_install_docker() {
   # Taken from an article at 
   # https://withblue.ink/2020/06/24/docker-and-docker-compose-on-raspberry-pi-os.html
   # Install some required packages first
-  sudo apt install -y apt-transport-https ca-certificates curl gnupg2 \
+  apt install -y apt-transport-https ca-certificates curl gnupg2 \
       software-properties-common
 
   # Get the Docker signing key for packages
   curl -fsSL \
       https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | \
-      sudo apt-key add -
+      apt-key add -
 
   # Add the Docker official repos
   echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
       $(lsb_release -cs) stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list
+      tee /etc/apt/sources.list.d/docker.list
 
   # Install Docker
-  sudo apt update
-  sudo apt install -y --no-install-recommends docker-ce cgroupfs-mount
+  apt update
+  apt install -y --no-install-recommends docker-ce cgroupfs-mount
 }
 
 for i in $*; do
@@ -300,6 +299,12 @@ echo SETUP COMPLETE
 #   - Add an option to let the installation script configure the wifi
 #     credentials, but we still need to suppor the creation of a temporary
 #     local SSID to accept config changes
+#   - Reset the default pi password
+
+# FOr every boot
+#   - Need to fdisk the data volume and check for errors and if they exist
+#     maybe we just blow it away and start over??  Need to do some testing
+#     with docker on whether that works or not (and )
 
 # TODO for a container/development installation:
 #   - Bring in extra packages for developemt time (packages-dev.txt)
